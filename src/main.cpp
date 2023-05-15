d30905ae5c6221d853fcbb172ca8b49e1d7910b7
amarelowo <willianaqw123@gmail.com>
2023-05-15 13:06:31 -0300

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,26 @@
 #include <PID.h>
 
 
-#define pinServo 33
+#define pinServoY 26
+#define pinServoX 14
+#define pinPotA 35
+#define pinPotB 34
 
-const int period = 50;
+const int period = 1;
 unsigned long time;
 
-float kp = 1;
-float ki = 0.5;
-float kd = 800;
-int setPoint = 500;
+float kp = 1.0;
+float ki = 0.0;
+float kd = 0.0;
+int setPoint = 250;
 
 float rangeMap = kp*setPoint;
 
-Servo s;
-PID pid(kp,ki,kd,setPoint, period);
+Servo sX;
+Servo sY;
+
+PID pidX(kp,ki,kd,setPoint, period);
+PID pidY(kp,ki,kd,setPoint, period);
 
 //String data = String(5);
 String data;
@@ -26,8 +32,14 @@ void setup(){
   Serial.begin(115200);
   Serial.setTimeout(10);
 
-  s.attach(pinServo);
-  s.write(120);
+  sX.attach(pinServoX);
+  sX.write(141);
+  sY.attach(pinServoY);
+  sY.write(148);
+
+  // pinMode(pinPotA, INPUT);
+  // pinMode(pinPotB, INPUT);
+
 
   time = millis();
 }
@@ -52,34 +64,71 @@ void loop(){
     data.trim();
     
     if (verificaDado(data) == false){
-      int dataForm = data.substring(2,7).toInt();
+      int dataX = data.substring(2,7).toInt();
+      int dataY = data.substring(8,13).toInt();
+
+      // sX.write(dataX);
+      // sY.write(dataY);
+      
+      // Serial.print("data: ");
+      // Serial.print(data);
+      // Serial.print(" | posX: ");
+      // Serial.print(dataX);
+      // Serial.print(" | posY: ");
+      // Serial.println(dataY);
+
+
 
       // s.write(dataForm);
       if (millis() > time+period){
       
+        
+
+        double posServoX = pidX.process(dataX);
+        double posServoY = pidY.process(dataY);
+
+        
+        int maxX = 151;  //15 graus 161
+        int minX = 131;  //-15 graus 121
+        int maxY = 158;  //15 graus 168
+        int minY = 138;  // -15 graus 128
+
+        posServoX = map(posServoX, -setPoint,setPoint,minX, maxX);
+        posServoY = map(posServoY, -setPoint,setPoint,minY, maxY);
+
+        if(posServoX > maxX){posServoX = maxX;}
+        if(posServoX < minX){posServoX = minX;}
+        if(posServoY > maxY){posServoY = maxY;}
+        if(posServoY < minY){posServoY = minY;}
+
+        // Serial.println(pidX.pidString() +" | pos: " + (String)posServo);
+        // Serial.print("posX: ");
+        // Serial.print(posServoX);
+        // Serial.print(" | posY: ");
+        // Serial.println(posServoY);
+
+        sY.write(posServoY);
+        sX.write(posServoX);
         time = millis();
-
-        double posServo = pid.process(dataForm);
-      
-
-        posServo = map(posServo, setPoint,-setPoint,0, 150);
-
-
-        if(posServo < 40){posServo = 40;}
-        if(posServo > 155){posServo = 155;}
-
-        Serial.println(pid.pidString() +" | pos: " + (String)posServo);
-
-        s.write(posServo+30);
       } 
     }
     else{
       kp = data.substring(2,7).toFloat();
       ki = data.substring(8,13).toFloat();
       kd = data.substring(14,18).toFloat();
-      pid.atualizarPID(kp,ki,kd);
+      pidX.atualizarPID(kp,ki,kd);
       Serial.println("kp: "+(String)kp+" | ki: "+(String)ki+" | kd: "+(String)kd);
     }
   }
-
+  // int leituraPotA = analogRead(pinPotA);
+  // int leituraPotB = analogRead(pinPotB);
+  // float posServoA = map(leituraPotA, 0,4096,0,180);
+  // float posServoB = map(leituraPotB,0,4096,0,180);
+  // if(posServoA < 78){
+  //   posServoA = 78;
+  // }
+  // if(posServoB < 98){
+  //   posServoB = 98;
+  // }
+  
 }
